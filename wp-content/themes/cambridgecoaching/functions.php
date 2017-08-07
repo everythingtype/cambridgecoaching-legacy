19142a3a69faa3787e3c7c091bede1f7af522a1d
Zack Rothauser <zrothauser@gmail.com>
2017-08-07 15:30:45 -0500

    Rename inc to includes

--- wp-content/themes/cambridgecoaching/functions.php
+++ wp-content/themes/cambridgecoaching/functions.php
@@ -5,16 +5,16 @@ define( 'THEME_VERSION', '0.1.0' );
 define( 'ASSETS_DIRECTORY', get_template_directory_uri() . '/assets/' );
 
 // Include main theme file
-include __DIR__ . '/inc/class-cambridge-coaching.php';
+include __DIR__ . '/includes/class-cambridge-coaching.php';
 
 // Include various theme hooks
-include __DIR__ . '/inc/class-hooks.php';
+include __DIR__ . '/includes/class-hooks.php';
 
 // Include theme components
-include __DIR__ . '/inc/class-breadcrumbs.php';
+include __DIR__ . '/includes/class-breadcrumbs.php';
 
 // Include other helper functions
-include __DIR__ . '/inc/helpers.php';
+include __DIR__ . '/includes/helpers.php';
 
 // Load the theme
 $cambridge_coaching = new Cambridge_Coaching( __FILE__ );
